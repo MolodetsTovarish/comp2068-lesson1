1ef1557b66fa93cc6de1637e6654dba5a5ddd19a
MolodetsTovarish <misha.okner@gmail.com>
2016-09-08 13:43:15 -0400

    end of class files

---
+++ helloworld.js
@@ -0,0 +1,3 @@
+// display a message
+console.log('Hello world');
+console.log('Does this print without a ; before it?'); // in this example, you remove the semicolon from the hello world message
---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+// launch the http server and send a response asynchronously
+http.createServer(function(request, response) {
+	response.writeHead(200); //status ok
+	response.end('First node page');
+}).listen(3000);
+
+console.log('Server running on port 3000');
